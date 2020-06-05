d31b9d04dfcc1e9b3fd899c3088646ae39adf0c8
Karthikraja.k <karthikraja.k@widas.in>
2020-06-05 12:01:40 +0530

    Update: Creating Cidaas Oauth2 Plugin

---
+++ src/CidaasOauth2Demo.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Cidaas\Oauth2Demo\CidaasOauth2Demo;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+
+class CidaasOauth2Demo extends Plugin
+{
+
+}
